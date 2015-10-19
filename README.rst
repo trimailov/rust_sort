22841c39192cbe55d84b716c9480dca2f9e33809
Justas T <j.trimailovas@gmail.com>
2015-10-20 00:58:54 +0300

    First implementation of bubble sort

---
+++ README.rst
@@ -0,0 +1,4 @@
+rust sort
+=========
+
+Just an exercise of writing some algorithms, to learn `rust <http://www.rust-lang.org/>`_.
---
+++ main.rs
@@ -0,0 +1,27 @@
+fn main() {
+    let mut arr = [8, 1, 2, 0, 9, 4, 7, 6, 3, 5];
+    bubble_sort(&mut arr);
+
+    for n in &arr {
+        println!("{}", n);
+    }
+}
+
+fn bubble_sort(arr: &mut [i32]) {
+    loop {
+        let mut swapped = false;
+        let length = arr.len() - 1;
+
+        for i in 0..length {
+            if arr[i+1] < arr[i] {
+                let temp = arr[i+1];
+                arr[i+1] = arr[i];
+                arr[i] = temp;
+                swapped = true;
+            }
+        }
+        if swapped == false {
+            break;
+        }
+    }
+}
